fd313458e6d8833e967f7a174f5b5a868326e4bf
erikmaraujodev <erikmaraujo1@gmail.com>
2026-07-16 09:38:23 -0300

    blabla home

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,15 @@
+import Home from "./pages/Home/Home";
+import { Routes, Route } from "react-router-dom";
+
 function App() {
   return (
     <>
       <div className="p-5 bg-cyan-950 text-gray-200 flex justify-center ">
         <h1 className="text-2xl font-bold">Projeto BASE</h1>
       </div>
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
     </>
   );
 }
